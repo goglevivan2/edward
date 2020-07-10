3d607e84757831cd839a8c382f95f1aa83a4fb46
ivan <ivangoglev@protonmail.com>
2020-07-11 00:04:56 +0300

    'all_tests'

--- tests.py
+++ tests.py
@@ -1,30 +1,30 @@
 import edward
 
-### TEST CASES ###
+### TEST CASE ###
 
-def test_case_001_simple():
+def test_001_simple():
     result = edward.Encryptor('files\\somefile.txt')
     assert result.return_decrypt_message() == 'edward'
 
-def test_case_002_big_file():
+def test_002_big_file():
     result = edward.Encryptor('files\\bigfile.txt')
     assert len(result.return_decrypt_message())==len('edward')*(100000//len('wweddaadwaffrdnn'))
 
-def test_case_003_error_file_name():
+def test_003_error_file_name():
     try:
         edward.Encryptor('files\\file_does_not_exist.txt') # ERROR
         assert False
     except:
         assert True
 
-def test_case_004_empty_file():
+def test_004_empty_file():
     result = edward.Encryptor('files\\emptyfile.txt')
     assert result.return_decrypt_message() == ''
 
-def test_case_005_normal_file():
+def test_005_normal_file():
     result = edward.Encryptor('files\\normalfile.txt')
     assert result.return_decrypt_message() == 'this file is not encrypted'
 
-def test_case_006_three_identical_letters():
+def test_006_three_identical_letters():
     result = edward.Encryptor('files\\file4tc6.txt')
     assert result.return_decrypt_message() == 'edward'
--- unittests.py
+++ unittests.py
@@ -1,14 +1,35 @@
 import unittest
 import edward
 
-class MyTestCase(unittest.TestCase):
-    def test_case_001_simple(self):
+class EdwardTestCase(unittest.TestCase):
+
+    def test_001_simple(self):
         result = edward.Encryptor('files\\somefile.txt')
         self.assertTrue(result.return_decrypt_message() == 'edward')
 
-    def test_case_002_big_file(self):
+    def test_002_big_file(self):
         result = edward.Encryptor('files\\bigfile.txt')
         self.assertTrue(len(result.return_decrypt_message()) == len('edward') * (100000 // len('wweddaadwaffrdnn')))
+
+    def test_003_error_file_name(self):
+        try:
+            edward.Encryptor('files\\file_does_not_exist.txt')  # ERROR
+            self.assertTrue(False)
+        except:
+            self.assertTrue(True)
+
+    def test_004_empty_file(self):
+        result = edward.Encryptor('files\\emptyfile.txt')
+        self.assertTrue(result.return_decrypt_message() == '')
+
+    def test_005_normal_file(self):
+        result = edward.Encryptor('files\\normalfile.txt')
+        self.assertTrue(result.return_decrypt_message() == 'this file is not encrypted')
+
+    def test_006_three_identical_letters(self):
+        result = edward.Encryptor('files\\file4tc6.txt')
+        self.assertTrue(result.return_decrypt_message() == 'edward')
+
 
 
 
