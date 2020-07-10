85b3b35da94eed77e11cda4929fecdf27b44ac6f
ivan <ivangoglev@protonmail.com>
2020-07-10 23:49:17 +0300

    'unittest2'

--- unittests.py
+++ unittests.py
@@ -6,5 +6,11 @@ class MyTestCase(unittest.TestCase):
         result = edward.Encryptor('files\\somefile.txt')
         self.assertTrue(result.return_decrypt_message() == 'edward')
 
+    def test_case_002_big_file(self):
+        result = edward.Encryptor('files\\bigfile.txt')
+        self.assertTrue(len(result.return_decrypt_message()) == len('edward') * (100000 // len('wweddaadwaffrdnn')))
+
+
+
 if __name__ == '__main__':
     unittest.main()
